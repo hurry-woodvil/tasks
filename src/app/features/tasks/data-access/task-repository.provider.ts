59a76632704a7a865555462aba14b33611e9664d
hurry-woodvil <hurry.woodvil@gmail.com>
2026-07-03 22:32:03 +0900

    refactor(data-access): TaskRepositoryProvider を設定可能に更新

--- src/app/features/tasks/data-access/task-repository.provider.ts
+++ src/app/features/tasks/data-access/task-repository.provider.ts
@@ -2,10 +2,13 @@ import { Provider } from '@angular/core';
 
 import { TASK_REPOSITORY } from './task-repository';
 import { JsonServerTaskRepository } from './json-server-task-repository';
+import { MockTaskRepository } from './mock-task-repository';
 
-export function provideTaskRepository(): Provider {
+type TaskRepositoryType = 'json-server' | 'mock';
+
+export function provideTaskRepository(type: TaskRepositoryType = 'json-server'): Provider {
   return {
     provide: TASK_REPOSITORY,
-    useClass: JsonServerTaskRepository,
+    useClass: type === 'mock' ? MockTaskRepository : JsonServerTaskRepository,
   };
 }
