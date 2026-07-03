1383c318b588ac28c50b0689e33987aeacb5bbec
hurry-woodvil <hurry.woodvil@gmail.com>
2026-07-03 14:02:18 +0900

    feat(app): Angular App の実装

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { TaskListPage } from '@tasks/pages/list-page/list-page';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'full',
+  },
+  {
+    path: 'tasks',
+    component: TaskListPage,
+  },
+];
