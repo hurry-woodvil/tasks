d481b44cf915553e6222cd2eb8532f99a9eee2ab
hurry-woodvil <hurry.woodvil@gmail.com>
2026-07-09 17:02:03 +0900

    feat(backend): auth, users modules を追加

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
