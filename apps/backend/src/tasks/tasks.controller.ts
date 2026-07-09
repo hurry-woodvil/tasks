408b84e9eaefd7737bae71d2a4af607608838b66
hurry-woodvil <hurry.woodvil@gmail.com>
2026-07-09 16:44:10 +0900

    docs(backend): Swagger API documentation を追加

--- apps/backend/src/tasks/tasks.controller.ts
+++ apps/backend/src/tasks/tasks.controller.ts
@@ -12,21 +12,43 @@ import type { Task } from './models/task';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import {
+  ApiBadRequestResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { TaskDto } from './dto/task.dto';
 
+@ApiTags('tasks')
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly taskService: TasksService) {}
 
+  @ApiOperation({ summary: 'タスク一覧を取得する' })
+  @ApiOkResponse({ type: TaskDto, isArray: true })
   @Get()
   async findAll(): Promise<Task[]> {
     return await this.taskService.findAll();
   }
 
+  @ApiOperation({ summary: 'タスクを作成する' })
+  @ApiOkResponse({ type: TaskDto })
+  @ApiBadRequestResponse({ description: '入力値が不正です' })
   @Post()
   async create(@Body() dto: CreateTaskDto): Promise<Task> {
     return await this.taskService.create(dto);
   }
 
+  @ApiOperation({ summary: '指定したタスクを更新する' })
+  @ApiOkResponse({ type: TaskDto })
+  @ApiBadRequestResponse({
+    description: '入力値が不正です',
+  })
+  @ApiNotFoundResponse({
+    description: 'タスクが見つかりません',
+  })
   @Patch(':id')
   async update(
     @Param('id') id: string,
@@ -35,11 +57,16 @@ export class TasksController {
     return await this.taskService.update(id, dto);
   }
 
+  @ApiOperation({ summary: '指定したタスクを削除する' })
+  @ApiNotFoundResponse({
+    description: 'タスクが見つかりません',
+  })
   @Delete()
   async deleteAll(): Promise<void> {
     return await this.taskService.deleteAll();
   }
 
+  @ApiOperation({ summary: '全タスクを削除する' })
   @Delete(':id')
   async delete(@Param('id') id: string): Promise<void> {
     return this.taskService.delete(id);
