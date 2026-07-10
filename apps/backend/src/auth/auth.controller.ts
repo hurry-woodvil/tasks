01dc8288cbef8f5a96f49c1b8f1e6c31df0afc12
hurry-woodvil <hurry.woodvil@gmail.com>
2026-07-10 17:25:29 +0900

    feat(backend): レスポンスを記載

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -8,6 +8,7 @@ import {
 import { SignupDto } from './dto/signup.dto';
 import { SigninDto } from './dto/signin.dto';
 import { AuthService } from './auth.service';
+import { AuthUserResponseDto } from './dto/auth-user-response.dto';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -22,7 +23,7 @@ export class AuthController {
     description: 'メールアドレスがすでに使用されています',
   })
   @Post('signup')
-  signup(@Body() dto: SignupDto) {
+  signup(@Body() dto: SignupDto): Promise<AuthUserResponseDto> {
     return this.authService.signup(dto);
   }
 
